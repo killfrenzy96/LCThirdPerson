56082d14444eae79a7656ba64b06473891554884
killfrenzy96 <evan298@gmail.com>
2024-11-21 21:24:25 +1100

    added missing patch file

---
+++ Patches/TooManyEmotesPatch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using TooManyEmotes.Patches;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCThirdPerson.Patches
+{
+    [HarmonyPatch(typeof(ThirdPersonEmoteController))]
+    internal class TooManyEmotesPatch
+    {
+        public static bool isPerformingEmote = false;
+        public static bool firstPersonEmotesEnabled = false;
+
+        [HarmonyPostfix]
+        [HarmonyPatch("OnStartCustomEmoteLocal")]
+        private static void OnStartCustomEmoteLocalPrepatch() // Called when starting emote
+        {
+            isPerformingEmote = true;
+            firstPersonEmotesEnabled = ThirdPersonEmoteController.firstPersonEmotesEnabled;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("OnStopCustomEmoteLocal")]
+        private static void OnStopCustomEmoteLocalPrepatch() // Called when ending emote and resetting
+        {
+            isPerformingEmote = false;
+            firstPersonEmotesEnabled = ThirdPersonEmoteController.firstPersonEmotesEnabled;
+            FixCamera();
+        }
+
+        private static void FixCamera()
+        {
+            if (ThirdPersonPlugin.Instance == null) return;
+            ThirdPersonPlugin.Instance.SetEnabled(!ThirdPersonPlugin.Instance.Enabled);
+            ThirdPersonPlugin.Instance.SetEnabled(!ThirdPersonPlugin.Instance.Enabled);
+        }
+    }
+}
